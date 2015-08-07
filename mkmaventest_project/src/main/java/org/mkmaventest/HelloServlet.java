b692dfe30c49ee1a1f68d7c050bf9486596e3a1e
Matt Kenny <matthewkenny@gmail.com>
2015-08-06 17:41:31 -0700

    example jetty

---
+++ mkmaventest_project/src/main/java/org/mkmaventest/HelloServlet.java
@@ -0,0 +1,20 @@
+// SEE: http://www.eclipse.org/jetty/documentation/9.2.6.v20141205/maven-and-jetty.html???
+
+package org.mkmaventest;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet
+{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("<h1>Hello Servlet</h1>");
+        response.getWriter().println("session=" + request.getSession(true).getId());
+    }
+}
